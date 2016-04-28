a941f67d2ee7351cb2b5377860c34e3d4cc56ea0
Edward Dal Santo <ejds001@gmail.com>
2016-04-28 18:50:26 -0400

    Add a full_title helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,21 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
+# Be sure to restart your server when you modify this file.
+
+# You can add backtrace silencers for libraries that you're using but don't
+# wish to see in your backtraces.
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
+
+# You can also remove all the silencers if you're trying to debug a problem
+# that might stem from framework code.
+# Rails.backtrace_cleaner.remove_silencers!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+  # order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
