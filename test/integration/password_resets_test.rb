6c286cbac679100a250202868e80384d6d8ac8b0
Edward Dal Santo <ejds001@gmail.com>
2016-05-11 18:07:33 -0400

    Add user microposts

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -7,7 +7,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "password reests" do
+  test "password resets" do
     get new_password_reset_path
     assert_template "password_resets/new"
     # Invalid email
@@ -57,5 +57,20 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_not flash.empty?
     assert_redirected_to user
+  end
+
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path, password_reset: { email: @user.email }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+         email: @user.email,
+         user: { password:              "foobar",
+                 password_confirmation: "foobar" }
+    assert_response :redirect
+    follow_redirect!
+    assert_match /expired/i, response.body
   end
 end
